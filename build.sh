1c6001ed8007e0e26b953c23f0c19e261cfb4b3f
sheikhreed <sheikhreed@protonmail.com>
2025-06-24 22:50:57 +0600

    Update build script to include custom message icons

--- build.sh
+++ build.sh
@@ -10,10 +10,11 @@ else
 fi
 
 # Use Python to resolve absolute paths
-ICON_FILE=$(uv run $PYTHON_EXEC -c "import os; print(os.path.abspath('inkpotro/icon/icon.png'))")
+ICON_FILE=$(uv run $PYTHON_EXEC -c "import os; print(os.path.abspath('inkpotro/icons/inkpotro.png'))")
 AUTH_UI=$(uv run $PYTHON_EXEC -c "import os; print(os.path.abspath('inkpotro/ui/authentication.ui'))")
 DASH_UI=$(uv run $PYTHON_EXEC -c "import os; print(os.path.abspath('inkpotro/ui/dashboard.ui'))")
 FONT_FILE=$(uv run $PYTHON_EXEC -c "import os; print(os.path.abspath('inkpotro/font/SolaimanLipi.ttf'))")
+ICONS_DIR=$(uv run $PYTHON_EXEC -c "import os; print(os.path.abspath('inkpotro/icons'))")
 MAIN_FILE=$(uv run $PYTHON_EXEC -c "import os; print(os.path.abspath('inkpotro/__main__.py'))")
 
 # Run PyInstaller to build the executable
@@ -24,7 +25,8 @@ uv run pyinstaller \
   --icon="$ICON_FILE" \
   --add-data "$AUTH_UI${SEP}ui" \
   --add-data "$DASH_UI${SEP}ui" \
-  --add-data "$ICON_FILE${SEP}icon" \
+  --add-data "$ICON_FILE${SEP}icons" \
   --add-data "$FONT_FILE${SEP}font" \
+  --add-data "$ICONS_DIR${SEP}icons" \
   -n Inkpotro \
   "$MAIN_FILE"
